46b7bcff29be0aae031c3d52086c46896fdd5afd
Vlad2301 <vlad2301gthb@gmail.com>
2023-05-11 13:09:49 +0300

    task 4 is ready

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (marks.length && (typeof this.excluded === "undefined")) {
+        this.marks.push(...marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (Array.isArray(this.marks) && this.marks.length) {
+        return this.marks.reduce((acc, currentMark) => { return acc + currentMark / this.marks.length }, 0);
+    } else {
+        return 0;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    this.subject = undefined;
+    this.marks = undefined;
+
+    this.excluded = reason;
 }
