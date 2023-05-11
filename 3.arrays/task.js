7e33134d8b1bf20a9949962eb0834f94def25e64
Vlad2301 <vlad2301gthb@gmail.com>
2023-05-11 12:34:38 +0300

    task 3 is ready

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,48 @@
 function compareArrays(arr1, arr2) {
-  
+    let array;
+    if (arr1.length >= arr2.length) {
+        array = arr1;
+    } else {
+        array = arr2;
+    }
+
+    return array.every((currentElement, index) => {
+        return arr1[index] === arr2[index];
+    });
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (users.length > 0) {
+        let genderUsers = users.filter(user => {
+            return user.gender === gender;
+        });
+
+        return genderUsers.reduce((accumulator, currentUser) => {
+            return accumulator + currentUser.age / genderUsers.length;
+        }, 0);
+    } else {
+        return 0;
+    }
 }
+
+
+const people = [
+    { firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской" },
+    { firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской" },
+    { firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский" },
+    { firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский" },
+    { firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский" },
+    { firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский" },
+    { firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской" },
+    { firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской" },
+    { firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской" },
+    { firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский" },
+    { firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской" },
+    { firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской" },
+    { firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской" },
+    { firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской" },
+]
+console.log(getUsersNamesInAgeRange(people, "мужской")); // 32
+console.log(getUsersNamesInAgeRange(people, "женский")); // 27.4
+console.log(getUsersNamesInAgeRange([], "женский")); // 0
+console.log(getUsersNamesInAgeRange(people, "инопланетянин")); // 0
